cce0de173dd9da710529f142b52580a9043ae95d
forty76 <47844675+forty76@users.noreply.github.com>
2024-12-17 13:30:27 +0100

    Update LittleFS_EspFileManager.cpp

--- src/LittleFS_EspFileManager.cpp
+++ src/LittleFS_EspFileManager.cpp
@@ -2,8 +2,8 @@
 // #include "page.h"
 #include "webPage.h"
 #include "FS.h"
-#include <SD.h>
-#include "SPI.h"
+//#include <SD.h>
+//#include "SPI.h"
 #include <LittleFS.h>
 
 EspFileManager::EspFileManager(/* args */)
