fec931bbc108fbbe0265f91290d9b1d6c970b71a
forty76 <47844675+forty76@users.noreply.github.com>
2024-12-17 19:23:16 +0100

    Update LittleFS_EspFileManager.cpp

--- src/LittleFS_EspFileManager.cpp
+++ src/LittleFS_EspFileManager.cpp
@@ -72,9 +72,7 @@ void EspFileManager::setServer(AsyncWebServer *server)
             AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", html_page, html_page_len);
             response->addHeader("Content-Encoding", "gzip");
             request->send(response);
-            Serial.println(" ##################   str_dataFS in file ##########################################");
-            Serial.println(str_dataFS);
-                
+                  
             // request->send(200, "text/html", html_page); 
             // request->send(200, "text/plain", "Test route working");
         });
@@ -84,28 +82,20 @@ void EspFileManager::setServer(AsyncWebServer *server)
         my_path = request->arg("path").c_str();
         FSlistDir(request->arg("path").c_str(), 0);
         request->send(200, "text/plain", str_dataFS);
-        Serial.println(" ################## get-folder-contents  str_dataFS in file ##########################################");
-        Serial.println(str_dataFS);      
+    
     });
 
     server->on("/upload", HTTP_POST, [&](AsyncWebServerRequest *request)
         { request->send(200, "application/json", "{\"status\":\"success\",\"message\":\"File upload complete\"}"); }, [&](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final){
         String file_path;
-
-
-
         file_path = "";
-        
         file_path += my_path+"/";
-        
-        Serial.println("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬   "+my_path);
         file_path += filename;
-        Serial.println("////////////////////////////////////   "+file_path);
+       
         if(!index)
         {
             DEBUGX("UploadStart: %s\n", file_path.c_str());
-            Serial.println(" ******************** upload ***********************************************");
-            Serial.println(file_path); //%s\n
+            
             if(LittleFS.exists(file_path)) 
             {
                 LittleFS.remove(file_path);
@@ -138,7 +128,7 @@ void EspFileManager::setServer(AsyncWebServer *server)
             request->send(400, "application/json", "{\"status\":\"error\",\"message\":\"Path not provided\"}");
             return;
         }
-        Serial.println(" ********************         Deleting File: "+ path);
+        
         DEBUGL2("Deleting File: ", path);
         if (LittleFS.exists(path)) 
         {
@@ -162,7 +152,7 @@ void EspFileManager::setServer(AsyncWebServer *server)
             request->send(400, "application/json", "{\"status\":\"error\",\"message\":\"Path not provided\"}");
             return;
         }
-        Serial.println(" ********************         Download File: "+ path);
+        
         DEBUGL2("Downloading File: ", path);
         if (LittleFS.exists(path)) 
         {
